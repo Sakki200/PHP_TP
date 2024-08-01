216be1b97bd591497fc27e3188997fd0abcbeb76
Sakki <nilkiller200@gmail.com>
2024-08-01 10:33:21 +0200

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>2024 TP.com Tous droit réservé</p>
+    <p>A propos | Politique de confidentialité</p>
+</footer>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>TP</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="../pages/home.php">Acceuil</a></li>
+                <li><a href="../pages/products.php">Produits</a></li>
+                <li><a href="../pages/promos">Promo</a></li>
+                <li><a href="../pages/categories.php">Catégories</a></li>
+                <li><a href="../pages/contact.php">Contact</a></li>
+                <li><a href="../pages/login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
